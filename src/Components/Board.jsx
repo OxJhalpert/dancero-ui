11110472675cf48baa946e35b65af9105fdcb13a
smarinrojas <nafhta@gmail.com>
2022-02-08 09:05:09 -0500

    paypal && stripe implementation

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -29,9 +29,8 @@ import Modal from './Modal';
 import Fab from '@mui/material/Fab';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { useStripe } from "@stripe/react-stripe-js";
 import { collection, query, where, getDocs, setDoc, addDoc, doc, getFirestore } from "firebase/firestore";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 
 
@@ -56,37 +55,39 @@ function StepNine({ data, connect, transferToken, goBackPage }) {
 
   // const [firestoreDB, setFirestoreDB] = useState();
   const app = initializeApp(firebaseConfig);
-
   var firestoreDB = getFirestore(app)
-  
+
+
+
+
   // setFirestoreDB({ firestoreDB : getFirestore(app) }) ;
   const dataTransferQuery = query(collection(firestoreDB, "dateTransfer"));
 
   const [exchangeRatio, setexchangeRatio] = useState()
-  const [imgNFT , setImgNFT] = useState("https://via.placeholder.com/150");
+  const [imgNFT, setImgNFT] = useState("https://via.placeholder.com/150");
   const [nftcodes, setNFTCodes] = useState([]);
   const [nftSelected, SetNFTSelected] = useState(null);
   const [priceToPay, setPriceToPay] = useState(0);
   const [totalCop, setTotalCop] = useState(0);
 
 
-  function getExchangeRate(){
+  function getExchangeRate() {
     fetch('https://api.exchangerate-api.com/v4/latest/USD')
-    .then(res => {
-      return res.json();
-    })
-    .then(res => {
-      setexchangeRatio(res.rates.COP);
-      calculatePrice(res.rates.COP);
-    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setexchangeRatio(res.rates.COP);
+        calculatePrice(res.rates.COP);
+      })
   }
-  
-  useEffect(()=>{
-      getExchangeRate();
-  },[setexchangeRatio])
 
   useEffect(() => {
-    if(data.user !== ""){
+    getExchangeRate();
+  }, [setexchangeRatio])
+
+  useEffect(() => {
+    if (data.user !== "") {
       loadNTFOfOwner();
     }
 
@@ -103,13 +104,28 @@ function StepNine({ data, connect, transferToken, goBackPage }) {
 
   ];
 
- 
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          id: "ID GENERADO POR FIREBASE",
+          summary : "Pack of hours",
+          amount: {
+            value: "150",
+          },
+        },
+      ],
+    });
+  };
+  const onApprove = (data, actions) => {
+    alert("Sucessfull payment.");
+    return actions.order.capture();
+  };
 
-  async function calculatePrice(exchangeRate)
-  {
+  async function calculatePrice(exchangeRate) {
     const roundTo = 5000;
     var cost = getPriceAndCostCalculation(data.Service, data.Level, data.Hours, data.City, data.Venue, data.place, 0);
-    var rest = cost[1] - cost[0]; 
+    var rest = cost[1] - cost[0];
     var totalCopRound = RoundTo(rest, roundTo)
     setTotalCop(totalCopRound);
     rest = totalCopRound / exchangeRate;
@@ -117,60 +133,81 @@ function StepNine({ data, connect, transferToken, goBackPage }) {
 
     setPriceToPay(rest);
   }
-  function RoundTo(number, roundTo)
-  {
-    return roundTo* Math.ceil(number /roundTo)
+  function RoundTo(number, roundTo) {
+    return roundTo * Math.ceil(number / roundTo)
   }
 
-  async function loadNTFOfOwner()
-  {
-    var danceNFTContract =  new window.web3.eth.Contract(danceNFT.abi, "0x3CAa1C35E5229EbbAEB70ea471F738a99c02381d");
+  async function loadNTFOfOwner() {
+    var danceNFTContract = new window.web3.eth.Contract(danceNFT.abi, "0x3CAa1C35E5229EbbAEB70ea471F738a99c02381d");
     var NFTCodes = await danceNFTContract.methods.getAllNFTCodesByAddress(data.user).call();
-    setNFTCodes(NFTCodes); 
+    setNFTCodes(NFTCodes);
   }
 
-   function storeTransaction(){
-    var idNewDoc = addDoc(collection(firestoreDB, "dateTransfer"), 
-    {
-      Venue : data.Venue,
-      City : data.City,
-      Place : data.place,
-      Teacher_gender : data.Gender,
-      Musical_genre : data.Musical_gender,
-      Service_type : data.Service,
-      Hours_pack : data.Hours,
-      Initia_lDate : initialDate,
-      Final_Date : finalDate,
-      payment : true 
-    });
+  function storeTransaction() {
+    var idNewDoc = addDoc(collection(firestoreDB, "dateTransfer"),
+      {
+        Venue: data.Venue,
+        City: data.City,
+        Place: data.place,
+        Teacher_gender: data.Gender,
+        Musical_genre: data.Musical_gender,
+        Service_type: data.Service,
+        Hours_pack: data.Hours,
+        Initia_lDate: initialDate,
+        Final_Date: finalDate,
+        payment: true
+      });
   }
 
-  async function loadNFTMetaData(nftId)
-  {
+  async function loadNFTMetaData(nftId) {
     const danceNFTContract = new window.web3.eth.Contract(danceNFT.abi, "0x3CAa1C35E5229EbbAEB70ea471F738a99c02381d")
     const balance = await danceNFTContract.methods
-    .balanceOf(data.user, nftId).call()
-    
-    if(balance >= 1){
+      .balanceOf(data.user, nftId).call()
+
+    if (balance >= 1) {
       var jsonData = await danceNFTContract.methods.uri(nftId).call();
       jsonData = jsonData.replace("{id}", nftId);
       fetch(jsonData)
         .then((response) => response.json())
         .then((data) => {
           setImgNFT(data.image);
-        });      
+        });
       alert('se ha aplicado un descuento')
-      
-    }else{
+
+    } else {
       alert('usted no es propietario del token')
     }
   }
 
+  const payWithStripe = () => {
+    fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [
+          { id : "FIREBASE CODE", name: "Pack Of " + data.Hours + " Hours", quantity: 1, priceInCents: priceToPay * 100 },
+        ],
+      }),
+    })
+      .then(res => {
+        if (res.ok) return res.json()
+        return res.json().then(json => Promise.reject(json))
+      })
+      .then(({ url }) => {
+        window.location = url
+      })
+      .catch(e => {
+        console.error(e.error)
+      })
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }} container spacing={2} p={2.5}>
-    <Grid container spacing={2} justifyContent="center">
-      <Grid item xs={12} md={4}>
-          <Card sx={{ mb: "1rem"}}>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12} md={4}>
+          <Card sx={{ mb: "1rem" }}>
             <CardHeader
               title={"Pack Of " + data.Hours + " Hours"}
               subheader={"Exchange rate " + (totalCop + " COP, ER " + exchangeRatio)}>
@@ -181,7 +218,7 @@ function StepNine({ data, connect, transferToken, goBackPage }) {
               </Typography>
             </CardContent>
           </Card>
-          <Card sx={{ mb: "1rem"}}>
+          <Card sx={{ mb: "1rem" }}>
             <CardContent>
               <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                 Pay with crypto
@@ -251,102 +288,83 @@ function StepNine({ data, connect, transferToken, goBackPage }) {
               </Button>
             </CardActions>
           </Card>
-          <Card sx={{ mb: "1rem"}}>
+          <Card sx={{ mb: "1rem" }}>
             <CardContent>
               <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                 Pay with FIAT
               </Typography>
             </CardContent>
             <CardActions>
-              {/* <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style={{margin :'20px'}}>
-              <input type="hidden" name="cmd" value="_xclick"/>
-              <input type="hidden" name="business" value="ivan9711@outlook.com"/>
-              <input type="hidden" name="lc" value="US"/>
-              <input type="hidden" name="amount" value={priceToPay}/>
-              <input type="hidden" name="currency_code" value="USD"/>
-              <input type="hidden" name="button_subtype" value="services"/>
-              <input type="hidden" name="no_note" value="0"/>
-              <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHostedGuest"/>
-              <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea."/>
-              <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1"/>
-              </form> */}
-{/* button pruebas  */}
-              <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top" style={{margin :'20px'}}>
-              <input type="hidden" name="cmd" value="_xclick"/>
-              <input type="hidden" name="business" value="ivan9711@outlook.com"/>
-              <input type="hidden" name="lc" value="US"/>
-              <input type="hidden" name="amount" value={priceToPay}/>
-              <input type="hidden" name="currency_code" value="USD"/>
-              <input type="hidden" name="button_subtype" value="services"/>
-              <input type="hidden" name="no_note" value="0"/>
-              <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHostedGuest"/>
-              <input type="image" src="https://www.paypalobjects.com/es_XC/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal, la forma más segura y rápida de pagar en línea."/>
-              <img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1"/>
-              </form>              
-
-              <Modal priceToPay={priceToPay} storeTransaction={storeTransaction}></Modal>
+              <PayPalScriptProvider options={{ 
+                "client-id": "AQ_Qiip-PYzDW2tacR6FO22P8DhfgIsvdBlANNwqCSQ-hTrVYCB7zrrZFocURXAS2d_S5CHqN9cRVaOK" }}>
+                <PayPalButtons style={{ layout: "horizontal" }}
+                      createOrder={(data, actions) => createOrder(data, actions)}
+                      onApprove={(data, actions) => onApprove(data, actions)}
+                 />
+              </PayPalScriptProvider>
+              <Button onClick={payWithStripe}>Pay with stripe</Button>
             </CardActions>
           </Card>
           <Fab variant="extended" size="medium" color="primary" onClick={() => goBackPage()} >
-                <ArrowBackIcon />
+            <ArrowBackIcon />
           </Fab>
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Card sx={{ maxWidth: '100%' }}>
-          <CardHeader
-            title="Load your NFT"
-          />
-          <CardMedia
-            component="img"
-            height="285"
-            image={imgNFT}
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <FormControl fullWidth>
-              {
-                nftcodes.length > 0 ?
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={nftSelected}
-                  onChange={(evt) => {SetNFTSelected(evt.target.value); loadNFTMetaData(evt.target.value); }} 
-                >
-                  { nftcodes.map( (nft) => <MenuItem value={nft}>{nft}</MenuItem>) }
-                </Select> :
-                  <b>Connect your wallet to load your NFTs</b>                  
-            }
-            </FormControl>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={12} md={4}>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ maxWidth: '100%' }}>
+            <CardHeader
+              title="Load your NFT"
+            />
+            <CardMedia
+              component="img"
+              height="285"
+              image={imgNFT}
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <FormControl fullWidth>
+                {
+                  nftcodes.length > 0 ?
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={nftSelected}
+                      onChange={(evt) => { SetNFTSelected(evt.target.value); loadNFTMetaData(evt.target.value); }}
+                    >
+                      {nftcodes.map((nft) => <MenuItem value={nft}>{nft}</MenuItem>)}
+                    </Select> :
+                    <b>Connect your wallet to load your NFTs</b>
+                }
+              </FormControl>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={4}>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 100 }} aria-label="simple table">
               <TableBody>
                 {rows.map((row) => (
-                <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                <TableCell component="th" scope="row">
-                    {row.name}
-                </TableCell>
-                <TableCell align="right">{row.option}</TableCell>
-                <TableCell align="right">{row.name2}</TableCell>
-                <TableCell align="right">{row.option2}</TableCell>
-                </TableRow>
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.option}</TableCell>
+                    <TableCell align="right">{row.name2}</TableCell>
+                    <TableCell align="right">{row.option2}</TableCell>
+                  </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
+        </Grid>
       </Grid>
-    </Grid>
-</Box>
+    </Box>
   )
 }
 
