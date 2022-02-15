1ba62e56d770d1128b9529a7b7d7dc1a26879484
Drd3 <37458534+Drd3@users.noreply.github.com>
2022-02-15 13:42:22 -0500

    New layout in ClassType & Location

--- src/Components/ClassTypeStep.jsx
+++ src/Components/ClassTypeStep.jsx
@@ -1,73 +1,47 @@
-import { Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import online from "../images/online.jpg";
 import studio from '../images/studio.jpg';
+import "../scss/cards.scss"
+import "../scss/layout.scss"
 
 
 
 function StepOne({ update, setPage  }) {
   return (
-    <Grid sx={{ flexGrow: 1 }} container spacing={2} p={2.5}>
-      <Grid item xs={12} >
-        <Grid container justifyContent="center" spacing={2}  alignItems="center">
+
+    <div className="container">
+      <div className="section-title">
           <h2>
             Welcome to Dancero wizard, please select one of the following
             options :
           </h2>
-        </Grid>
-      </Grid>
-      <Grid item xs={12}>
-        <Grid container justifyContent="center" spacing={3}>
-          <Grid key={1} item>
-            <Card
-              sx={{ maxWidth: 300 }}
-              onClick={() => {
-                // setPage(3)
-                update("Venue", "Online")}
-              }
-            >
-              <CardMedia
-                component="img"
-                alt="Online"
-                height="300"
-                image={online}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Online
-                </Typography>
-              </CardContent>
-              <CardActions></CardActions>
-            </Card>
-          </Grid>
-          <Grid key={3} item>
-            <Card
-              sx={{ maxWidth: 300 }}
-              onClick={() =>   
-              update("Venue", "Offline")}
-              
-            >
-              <CardMedia
-                component="img"
-                alt="Studio"
-                height="300"
-                image={studio}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Offline
-                </Typography>
-              </CardContent>
-              <CardActions></CardActions>
-            </Card>
-          </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      </div>
+      <div className="cards_container">
+          <div className="card" 
+            onClick={() => {
+              // setPage(3) 
+              update("Venue", "Online")}}> 
+            <div className="card_image">
+              <img src={online}/>
+            </div>
+            <div className="card_name">
+                Online
+            </div>
+          </div>
+          <div className="card"
+            sx={{ maxWidth: 300 }}
+            onClick={() =>   
+            update("Venue", "Offline")}
+            
+          >
+            <div className="card_image">
+              <img src={studio}/>
+            </div>
+            <div className="card_name">
+                Studio
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
--- src/Components/LocationStep.jsx
+++ src/Components/LocationStep.jsx
@@ -8,104 +8,77 @@ import bogota from "../images/bogota.jpg";
 import cartagena from "../images/cartagena.jpeg";
 import medellin from "../images/medellin.jpg";
 import Fab from '@mui/material/Fab';
+import "../scss/cards.scss"
+import "../scss/layout.scss"
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 function StepFour({ data,update, goBackPage,setPage }) {
     return (
-      <Grid sx={{ flexGrow: 4 }} container spacing={2} p={2.5}>
-        <Grid item xs={12}>
-          <Grid container justifyContent="center" spacing={2}>
-          <Grid item xs={12}>
-              <Grid container justifyContent="center" spacing={2}>
-                <h2>Please select your city :</h2>
-              </Grid>
-            </Grid>
-            <Grid key={8} item>
-              <Card sx={{ maxWidth: 200 }} onClick={() => {
+      <div className="container">
+            <div className="section-title">
+              <h2>Please select your city :</h2>
+            </div>
+
+            <div className="cards_container">
+              <div className="card location-card" onClick={() => {
                 if(data.Venue === 'Online')
                 {
                   setPage(3)
                 }update("City", "Cartagena")}}>
-                <CardMedia
-                  component="img"
-                  alt="cartagena"
-                  height="300"
-                  image={cartagena}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                <div className="card_image">
+                  <img src={cartagena}/>
+                </div>
+                <div className="card_name">
                     Cartagena
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid key={9} item>
-              <Card sx={{ maxWidth: 200 }} onClick={() => {
+                </div>
+              </div>
+              <div className="card" onClick={() => {
                 if(data.Venue === 'Online')
                 {
                   setPage(3)
                 }update("City", "Bogota")}}>
-                <CardMedia
-                  component="img"
-                  alt="bogota"
-                  height="300"
-                  image={bogota}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+
+                <div className="card_image">
+                  <img src={bogota}/>
+                </div>
+                <div className="card_name">
                     Bogota
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid key={10} item>
-              <Card sx={{ maxWidth: 200 }} onClick={() => 
+                </div>
+              </div>
+              <div className="card" onClick={() => 
                 {
                   if(data.Venue === 'Online')
                 {
                   setPage(3)
                 }update("City", "Medellin")}}>
-                <CardMedia
-                  component="img"
-                  alt="medellin"
-                  height="300"
-                  image={medellin}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                <div className="card_image">
+                  <img src={medellin}/>
+                </div>
+                <div className="card_name">
                     Medellin
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid key={11} item>
-              <Card sx={{ maxWidth: 200 }} onClick={() => 
+                </div>
+              </div>
+              <div className="card" onClick={() => 
                 {if(data.Venue === 'Online')
                 {
                   setPage(3)
                 }
                   update("City", "Cali")}}>
-                <CardMedia 
-                  component="img" 
-                  alt="cali" 
-                  height="300" 
-                  image={cali} />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                <div className="card_image">
+                  <img src={cali}/>
+                </div>
+                <div className="card_name">
                     Cali
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
+                </div>
+              </div>
+            </div>
             <Grid xs={12} item container justifyContent="center" >
             <Fab variant="extended" size="medium" color="primary" onClick={() => goBackPage()} >
               <ArrowBackIcon />
             </Fab>
             </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      </div>
     );
   }
 
